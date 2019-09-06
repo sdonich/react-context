6d956d27614610477b76321ee04600f08a5ff373
Sergei <sdonich@gmail.com>
2019-09-06 14:14:46 +0200

    Initial first stage and add primary markup

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => this.setState({ language });
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us"
+            onClick={() => { this.onLanguageChange('english') }}
+          />
+          <i className="flag nl"
+            onClick={() => { this.onLanguageChange('dutch') }}
+          />
+          <UserCreate />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
