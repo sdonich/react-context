672fcc65242ba6ce6e56b8c8f8a9233b4bea5c96
sdonich <sdonich@gmail.com>
2019-09-06 18:25:24 +0200

    Some code spliting

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -11,20 +12,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i className="flag us"
-            onClick={() => { this.onLanguageChange('english') }}
-          />
-          <i className="flag nl"
-            onClick={() => { this.onLanguageChange('dutch') }}
-          />
-          <LanguageContext.Provider value={this.state.language}>
-            <ColorContext.Provider value="primary">
-              <UserCreate />
-            </ColorContext.Provider>
-          </LanguageContext.Provider>
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+        <LanguageContext.Provider value={this.state.language}>
+          <ColorContext.Provider value="primary">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageContext.Provider>
       </div>
     );
   }
