826a348528b38c6113c0bf73df7b81c32d485909
sdonich <sdonich@gmail.com>
2019-09-06 18:56:20 +0200

    Add some sort of redux store

--- src/components/Field.js
+++ src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = languageContext;
 
   render() {
-    const text = this.context === 'english' ? 'Name' : 'Naam';
+    const text = this.context.language === 'english' ? 'Name' : 'Naam';
     return (
       <div className="ui field">
         <label>{text}</label>
